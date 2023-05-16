5f16dda93a205e10136d302030d8047451b44d85
Ryan J Owens <rowens794@gmail.com>
2023-05-16 08:37:09 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -136,7 +136,7 @@ export default function Home({ data, certs }: { data: any; certs: any }) {
   };
 
   return (
-    <main className={`min-h-screen p-12 bg-gray-800`}>
+    <main className={`min-h-screen p-12 bg-gray-800 w-full`}>
       <Link
         href="https://www.wvsao.gov/CountyCollections/Default#SB552LandSalesListings"
         className="text-gray-400 underline text-sm mr-4"
